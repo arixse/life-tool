173c66d61d1a5f34db03dcf8dd3827e6506e878d
fusheng <fusheng@netposa.com>
2019-06-05 11:29:39 +0800

    fix

--- services/os.js
+++ services/os.js
@@ -7,7 +7,7 @@ module.exports = function() {
 	let freeMem = (os.freemem() / Math.pow(1024, 3)).toFixed(2);
     let totalMem = (os.totalmem() / Math.pow(1024, 3)).toFixed(2);
 	let usedMem = (totalMem-freeMem).toFixed(2);
-	console.log("当前系统版本:" + osVersion);
+	console.log(chalk.green("当前系统版本:" + osVersion));
 	console.log(chalk.cyan('系统内存使用情况:'))
 	console.log(chalk.cyan(Gauge(usedMem,totalMem,30,totalMem*0.8,usedMem+'GB')))
 };
