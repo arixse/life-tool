127be013c2c9ccb0dee66394f2b0bf03a7c03e48
fusheng <fusheng@netposa.com>
2019-06-05 11:25:15 +0800

    os tool

--- services/os.js
+++ services/os.js
@@ -1,13 +1,13 @@
 const os = require("os");
-const Table = require('cli-table');
 const chalk = require("chalk").default;
+const clui = require('clui');
+const Gauge = clui.Gauge;
 module.exports = function() {
 	let osVersion = os.platform() + " " + os.release();
-	let freeMem = (os.freemem() / Math.pow(1024, 3)).toFixed(2) + "GB";
-    let totalMem = (os.totalmem() / Math.pow(1024, 3)).toFixed(2) + "GB";
-    
-	console.log("os:" + osVersion);
-	let table = new Table({ head: ["FreeMemory", "TotalMemory"] });
-	table.push([freeMem, totalMem]);
-	console.log(chalk.cyan(table.toString()));
+	let freeMem = (os.freemem() / Math.pow(1024, 3)).toFixed(2);
+    let totalMem = (os.totalmem() / Math.pow(1024, 3)).toFixed(2);
+	let usedMem = (totalMem-freeMem).toFixed(2);
+	console.log("当前系统版本:" + osVersion);
+	console.log(chalk.cyan('系统内存使用情况:'))
+	console.log(chalk.cyan(Gauge(usedMem,totalMem,30,totalMem*0.8,usedMem+'GB')))
 };
