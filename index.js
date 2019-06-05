16f1d78540350c57cf5151b64a32382ca0718788
fusheng <fusheng@netposa.com>
2019-06-05 10:35:38 +0800

    --help

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 #! /usr/bin/env node
 const program = require("commander");
 const chalk = require("chalk").default;
-const services = require('./services/index')
+const services = require('./services/index');
+const figlet = require('figlet');
 program
 	.version("1.0.0")
 	.alias("lt")
 	.description("The life tools,您的生活小帮手")
+	.description(chalk.cyan(
+		figlet.textSync('The life tool',{
+			font:'Ghost',
+			horizontalLayout:'default',
+			verticalLayout:'default',
+		})
+	))
 	.action(option => {
 		services(option);
 	})
