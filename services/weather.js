19e5f64106592d96e0c4cde9d4ecf604eb9611c2
fusheng <fusheng@netposa.com>
2019-06-05 14:34:22 +0800

    fix weather

--- services/weather.js
+++ services/weather.js
@@ -1,30 +1,41 @@
-const moment = require('moment')
+const moment = require("moment");
 const chalk = require("chalk").default;
 const inquirer = require("inquirer");
-const api = require('../http/index');
-const clui = require('clui');
-
+const api = require("../http/index");
+const clui = require("clui");
+const blessed = require("blessed");
+const contrib = require("blessed-contrib");
 const weatherQuestion = {
-    type:'input',
-    name:'city',
-    message:'请输入城市名称'
-}
+	type: "input",
+	name: "city",
+	message: "请输入城市名称"
+};
 function weatherInput() {
-    inquirer.prompt([weatherQuestion]).then(res=>{
-        if(!res.city){
-            console.log(chalk.red('城市名称错误，请重新输入'));
-            weatherInput();
-            return;
-        }
-        let spinner = new clui.Spinner('正在查询中...',['⣾','⣽','⣻','⢿','⡿','⣟','⣯','⣷']);
-        spinner.start();
-        api.fetchFiveDaysWeather(res.city).then(weathers=>{
-            spinner.stop();
+	inquirer.prompt([weatherQuestion]).then(res => {
+		if (!res.city) {
+			console.log(chalk.red("城市名称错误，请重新输入"));
+			weatherInput();
+			return;
+		}
+		let spinner = new clui.Spinner("正在查询中...", [
+			"⣾",
+			"⣽",
+			"⣻",
+			"⢿",
+			"⡿",
+			"⣟",
+			"⣯",
+			"⣷"
+		]);
+		spinner.start();
+		api.fetchFiveDaysWeather(res.city).then(weathers => {
+			spinner.stop();
             let date = moment().format("YYYY-MM-DD :HH:mm:ss");
-            console.log(weathers);
-        })
-    })
+            console.log(chalk.bgCyan.white(`当前系统时间:`+date));
+            weathers.forEach(item=>{
+                console.log(chalk.yellow(`${item.week}:${item.weather},${item.wind},${item.winp},${item.temp_low}℃  ~  ${item.temp_high}℃`))
+            })
+		});
+	});
 }
-module.exports = function(city) {
-    weatherInput();
-}
+module.exports = weatherInput;
